fa785c65ca60a2d6498e3c251096d5e8bd92b545
Man1710147 <babubhaimevasa@gmil.com>
2024-06-24 16:36:42 +0530

    CH-2 : introduction t c languge

---
+++ CH-2/lw_2_3_1.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+
+{    int a=12,b=6;
+     clrscr();
+     printf("addition of\t\t %d + %d:%d\n",a,b,a+b);
+     printf("subtraction of\t\t %d - %d:%d\n",a,b,a-b);
+     printf("multiplication of\t %d * %d:%d\n",a,b,a*b);
+     printf("division of\t\t %d / %d:%d\n",a,b,a/b);
+     printf("modulo of\t\t %d % %d:%d\n",a,b,a%b);
+
+     getch();
+
+}
