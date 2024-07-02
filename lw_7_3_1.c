b4afe6d21c725f97cd2d9f5609c213647b891c7e
Man1710147 <babubhaimevasa@gmail.com>
2024-07-02 17:16:30 +0530

    Add files via upload

---
+++ lw_7_3_1.c
@@ -0,0 +1,490 @@
+#include<stdio.h>
+#define P printf
+#define S scanf
+
+main()
+{
+    int a,b,c,d,e,f,g;
+
+    P("Enter The Number Of A:");
+    S("%d",&a);
+
+    P("Enter The Number Of B:");
+    S("%d",&b);
+
+    P("Enter The Number Of C:");
+    S("%d",&c);
+
+    P("Enter The Number Of D:");
+    S("%d",&d);
+
+    P("Enter The Number Of E:");
+    S("%d",&e);
+
+    P("Enter The Number Of F:");
+    S("%d",&F);
+
+    P("Enter The Number Of G:");
+    S("%d",&g);
+
+    if(a>b)
+    {
+        //a
+        if(a>c)
+        {
+            if(a>d)
+            {
+                if(a>e)
+                {
+                    if(a>f)
+                    {
+                        if(a>g)
+                        {
+                            P("A is big");
+                        }
+                        else
+                        {
+                            P{"G is big"}
+                        }
+                    }
+                    else
+                    {
+                        //f
+                        if(f>g)
+                        {
+                            P("F is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                }
+                else
+                {
+                    //e
+                    if(e>f)
+                    {
+                        if(e>g)
+                        {
+                            P("E is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                    else
+                    {
+                        //f
+                        if(f>g)
+                        {
+                            P("F is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                }
+            }
+            elsr
+            {
+                //d
+                if(d>e)
+                {
+                    if(d>f)
+                    {
+                        if(d>g)
+                        {
+                            P("D is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                    else
+                    {
+                        // f
+                        if(f>g)
+                        {
+                            P("F is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                }
+                else
+                {
+                    // e
+                    if(e>f)
+                    {
+                        if(e>g)
+                        {
+                            P("E is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                    else
+                    {
+                        // f
+                        if(f>g)
+                        {
+                            P("F is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            //c
+            if(c>d)
+            {
+                if(c>e)
+                {
+                    if(c>f)
+                    {
+                        if(c>g)
+                        {
+                            P("C is big");
+                        }
+                        else
+                        {
+                            ("G is big");
+                        }
+                    }
+                    else
+                    {
+                        // f
+                         if(f>g)
+                        {
+                            P("F is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                }
+                else
+                {
+                    // e
+                    if(e>f)
+                    {
+                        if(e>g)
+                        {
+                            P("E is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                    else
+                    {
+                        // f
+                        if(f>g)
+                        {
+                            P("F is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                // d
+                if(d>e)
+                {
+                    if(d>f)
+                    {
+                        if(d>g)
+                        {
+                            P("D is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                    else
+                    {
+                        // f
+                        if(f>g)
+                        {
+                            P("F is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                }
+                else
+                {
+                    // e
+                    if(e>f)
+                    {
+                        if(e>g)
+                        {
+                            P("E is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                }
+            }
+        }
+    }
+  }  
+        else
+        {
+        //b
+        if(b>c)
+        {
+            if(b>d)
+            {
+                if(b>e)
+                {
+                    if(b>f)
+                    {
+                        if(b>g)
+                        {
+                            P("B is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                    else
+                    {
+                        // f
+                        if(f>g)
+                        {
+                            P("F is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                }
+                else
+                {
+                    // e
+                    if(e>f)
+                    {
+                        if(e>g)
+                        {
+                            P("E is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                    else
+                    {
+                        //f
+                        if(f>g)
+                        {
+                            P("F is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                // d
+                if(d>e)
+                {
+                    if(d>f)
+                    {
+                        if(d>g)
+                        {
+                            P("D is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                    else
+                    {
+                        // f
+                        if(f>g)
+                        {
+                            P("F is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                }
+                else
+                {
+                    // e
+                    if(e>f)
+                    {
+                        if(e>g)
+                        {
+                            P("E is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                    else
+                    {
+                        // f
+                        if(f>g)
+                        {
+                            P("F is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                }   
+            }
+        }
+        else
+        {
+            // c
+             if(c>d)
+            {
+                if(c>e)
+                {
+                    if(c>f)
+                    {
+                        if(c>g)
+                        {
+                            P("C is big");
+                        }
+                        else
+                        {
+                            ("G is big");
+                        }
+                    }
+                    else
+                    {
+                        // f
+                         if(f>g)
+                        {
+                            P("F is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                }
+                else
+                {
+                    // e
+                    if(e>f)
+                    {
+                        if(e>g)
+                        {
+                            P("E is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                    else
+                    {
+                        // f
+                        if(f>g)
+                        {
+                            P("F is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                // d
+                if(d>e)
+                {
+                    if(d>f)
+                    {
+                        if(d>g)
+                        {
+                            P("D is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                    else
+                    {
+                        // f
+                        if(f>g)
+                        {
+                            P("F is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                }
+                else
+                {
+                    // e
+                    if(e>f)
+                    {
+                        if(e>g)
+                        {
+                            P("E is big");
+                        }
+                        else
+                        {
+                            P("G is big");
+                        }
+                    }
+                }
+            }
+        }
+        }
+    else    
+    {
+        
+    }
+
+
+    
+    
+
+
